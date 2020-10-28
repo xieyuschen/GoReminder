6f255544b19dbdab0b54eafe8e4c8f29c7431f19
Xieyuschen <xieyuschen@gmail.com>
2020-10-28 20:59:27 +0800

    Get Body by http get

--- WebScanner/scanner.go
+++ WebScanner/scanner.go
@@ -1,1 +1,17 @@
 package WebScanner
+
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func GetPage(url string) (pageContent string){
+	resp,err:=http.Get(url)
+	if err!=nil{
+		log.Panic(err)
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+
+	return string(body)
+}
--- main.go
+++ main.go
@@ -1,1 +1,8 @@
-package GoReminder
+package main
+
+import "GoReminder/WebScanner"
+
+func main(){
+	str := "https://studygolang.com/articles/9194"
+	WebScanner.GetPage(str)
+}
