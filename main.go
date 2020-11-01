e4fcde4a1cc45876ef4a33e8dae711e812ae9a5a
Xieyuschen <Xieyuschen@users.noreply.github.com>
2020-11-01 14:43:00 +0800

    try recover

--- main.go
+++ main.go
@@ -1,8 +1,28 @@
 package main
 
-import "GoReminder/Reminder"
+import (
+	"fmt"
+	"log"
+)
 
+func DoPanic(){
+	log.Panic("Just For try")
+}
+func Recover(){
+	fmt.Println("Recover plays its role")
+	recover()
+}
+func try()  {
+	for{
+		defer Recover()
+		DoPanic()
+
+	}
+}
 func main(){
-	url :="http://www.biquge.se/23609/"//77693356.html"
-	Reminder.Reminder(url)
+	//url :="http://www.biquge.se/23609/"//77693356.html"
+	//Reminder.Reminder(url)
+	try()
+	fmt.Println("Helloworld")
+	try()
 }
